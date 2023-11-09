7f6ffcf2f79207c9078e71ff712e54b299631644
eigen98 <68258365+eigen98@users.noreply.github.com>
2023-11-09 10:40:12 +0900

    Title: 1로 이루어진 집합의 크기, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231109/1로 이루어진 집합의 크기/size-of-set-of-1s.swift
+++ 231109/1로 이루어진 집합의 크기/size-of-set-of-1s.swift
@@ -1,3 +1,67 @@
+let nm = readLine()!.split(separator: " ").map{Int(String($0))!}
+let n = nm[0] 
+let m = nm[1]
+var arr = [[Int]]()
+
+var visited = [[Bool]](repeating: [Bool](repeating: false, count: m), count: n)
+
+let dir = [[1,0], [0,1], [-1,0], [0, -1]]
+var zeroPosition = [[Int]]()
+for i in 0..<n{
+    var col = 0
+    let input = readLine()!.split(separator: " ").map{Int(String($0))!}
+    arr.append(input)
+}
+for i in 0..<n{
+    for j in 0..<m{
+        var num = arr[i][j]
+        if num == 0 {
+            zeroPosition.append([i, j])
+        }
+    }
+}
+
+var maxCount = 0
+var preRow = -1
+var preCol = -1
+//전부다 1인 경우?
+if n * m == zeroPosition.count { 
+    print(n * m) 
+}else if zeroPosition.count == 0{
+    print(1)
+}else{
+
+
+    for info in zeroPosition{
+        if preRow >= 0 {
+            arr[preRow][preCol] = 0
+        }
+        var r = info[0]
+        var c = info[1]
+
+        arr[r][c] = 1
+        preRow = r
+        preCol = c 
+
+        for i in 0..<n{
+            for j in 0..<m{
+                visited[i][j] = false
+            }
+        }
+        
+        for i in 0..<n{
+            for j in 0..<m{
+                if visited[i][j] { continue }
+                if arr[i][j] == 0 { continue }
+                let count = bfs(row: i, col: j)
+                maxCount = max(count, maxCount)
+            }
+        }
+        
+    }
+    print(maxCount)
+}
+
 func bfs(row: Int, col: Int) -> Int{
     var count = 0
     var queue = [[Int]]()
