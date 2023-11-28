8234f2bc75b9c837b6e86ae765f36b59d5cf1036
eigen98 <68258365+eigen98@users.noreply.github.com>
2023-11-28 19:09:17 +0900

    Title: 색종이 2, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231128/색종이 2/color-paper-2.swift
+++ 231128/색종이 2/color-paper-2.swift
@@ -8,8 +8,8 @@ for i in 1...n{
     var xy = readLine()!.split(separator: " ").map{Int(String($0))!}
     var row = 500 - xy[1]
     var col = xy[0]
-    for i in (row - 10)...row{
-        for j in col...(col + 10){
+    for i in (row - 10)..<row{
+        for j in col..<(col + 10){
             arr[i][j] = 1
         }
     }
@@ -25,26 +25,28 @@ for i in 0...500{
 
 func dfs(r: Int, c: Int){
     if visited[r][c] {
-        return
+        return;
     }
     visited[r][c] = true
+    if r == 0 || c == 0 || r == 500 || c == 500{
+        answer += 1;
+        return
+    }
 
-    for i in 0...3 {
-        let nr = r + dir[i][0]
-        let nc = c + dir[i][1]
-
-        if nr < 0 || nr > 500 || nc < 0 || nc > 500 {
-            answer += 1
+    for i in 0...3{
+        var nr = r + dir[i][0]
+        var nc = c + dir[i][1]
+        if nr < 0 || nr >= 501 || nc < 0 || nc >= 501{
             continue
         }
 
+        if visited[nr][nc] { continue }
         if arr[nr][nc] == 0 {
             answer += 1
-        } else if !visited[nr][nc] {
-            dfs(r: nr, c: nc)
+            continue
         }
     }
-}
 
+}
 
 print(answer)
